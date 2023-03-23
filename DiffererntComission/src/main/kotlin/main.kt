04fcca48215f908f1c0bbd5637a9e8e47ae6f20e
twin730 <twin730@mail.ru>
2023-03-24 00:52:48 +0300

    main.kt finally changed

--- DiffererntComission/src/main/kotlin/main.kt
+++ DiffererntComission/src/main/kotlin/main.kt
@@ -1,13 +1,37 @@
-fun main() {
-    var amount = 50
-    var comissionAmount = amount * 0.075
+public fun main() {
 
-    println("Сумма перевода: " + (amount) + " рублей")
-    println("Сумма комиссии за перевод: " + (minimumComissionCheck(comissionAmount)) + " рублей")
 
+    var comission = comissionAmount("Visa")//Введите тип карты
+    println("Сумма комиссии за перевод: " + (comission) + " руб.")
 }
 
-fun minimumComissionCheck(comissionAmount: Double): Double {
-    return if (comissionAmount >= 35.0) comissionAmount else 35.0
+fun comissionAmount(cardType: String): Double {
+    return when {
+        (cardType == "MasterCard" || cardType == "Maestro") -> MasterCardMaestro(35_000, 10_000)
+        (cardType == "Visa" || cardType == "Мир") -> VisaMir(700, 18_000)
+        (cardType == "VKpay") -> VKpay(5_000, 120_000)
+        else -> 0.0
+    }
+}
 
+fun MasterCardMaestro(transferAmount: Int, previousMonthTransferAmount: Int): Double {
+    return when {
+        (transferAmount + previousMonthTransferAmount > 75000) || (transferAmount < 150000) -> (transferAmount / 100 * 0.6) + 20//до 75000 в мес комиссия 0, если больше 0,6% +20 руб)
+        else -> 0.0
+    }
+}
+
+fun VisaMir(transferAmount: Int, previousMonthTransferAmount: Int): Double {
+    return when {
+        (transferAmount / 100 * 0.75) > 35.0 -> transferAmount / 100 * 0.75
+        (transferAmount + previousMonthTransferAmount > 600_000) || (transferAmount < 150000) -> (transferAmount / 100 * 0.75)//0.75 % мин 35 руб
+        else -> 35.0
+    }
+}
+
+fun VKpay(transferAmount: Int, previousMonthTransferAmount: Int): Double {
+    return when {
+        (transferAmount + previousMonthTransferAmount > 600_000) || (transferAmount < 150000) -> 0.0
+        else -> 0.0
+    }
 }
